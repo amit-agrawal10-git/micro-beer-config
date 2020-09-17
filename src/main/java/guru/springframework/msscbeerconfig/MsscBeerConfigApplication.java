d2fc54402fa1fb788bd283ee9cf620ce61ca3a8c
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-09-17 22:20:30 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/msscbeerconfig/MsscBeerConfigApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsscBeerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerConfigApplication.class, args);
+	}
+
+}
